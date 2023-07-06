a66619808d7eb8046072474ec053f6884330a366
Ecan ESE <ercan.ese@bilgeadam.com>
2023-07-06 15:49:54 +0300

    First Commit

---
+++ Day1-Part1.ps1
@@ -0,0 +1,81 @@
+﻿get-service -Name KeyIso -DependentServices
+
+Get-EventLog -LogName Application
+Get-EventLog
+
+Get-Service -Name ALG
+Stop-Service -Name ALG
+Start-Service -Name ALG
+
+
+
+#Bana makine üzerinde Winrm,ALG,BITS ve wuauserv servislerini ekranda gösteren cmdleti yazın.
+Get-Service -Name ALG,BITS,Winrm,Wuauserv
+#BITS Servisini durduralım ve tekrar powershell ile başlatalım.
+Get-Service -Name BITS
+Stop-Service -Name BITS 
+Start-Service -Name BITS
+#BITS Servisinin Set komutu ile StarTtype değerini Automatic olarak güncelleyelim.
+Set-Service -Name BITS -StartupType Automatic
+
+
+Get-Help
+Get-Help -Name get-service
+Get-Help -Name Set-Service -Examples
+
+Get-Help -Name Set-Service -Full
+
+
+Get-Help -Name Stop-Process -Full
+
+Stop-Process -Name notepad
+
+$env:COMPUTERNAME
+$env:PSModulePath
+
+Get-Module
+Get-Module -ListAvailable
+Get-NetIPAddress
+
+New-AzSignalR
+
+Install-Module -Name SqlServer
+
+
+Get-Command -Verb get -Noun *IpAddress*
+Get-Help -Name Get-NetIPAddress -Full
+
+Get-NetIPAddress
+Get-Command -Module SqlServer
+
+Get-Command -Verb Get -Noun *Firewall*
+
+Get-Command -Name *firewall*
+
+Get-NetFirewallRule
+
+
+#Makine üzerinde dnscacheleri listeleyen komutu bularak calıstırın.
+Get-Command -Verb get -Noun "*cache*"
+Get-DnsClientCache
+#Makine üzerinde diskleri listeleyen komutu bulun ve çalıştırın.
+Get-Command -Verb get -Noun *disk*
+Get-Disk
+Get-PhysicalDisk 
+
+#Makine üzerinde Volumeleri listeyen komutu bulun ve çalıştırın.
+Get-Command -Verb get -Noun "*volume*"
+Get-Volume
+
+#C:\ altına Temp3 adında bir directory item olusturan cmdleti yazın.
+
+Get-Command -Verb * -Noun *Item*
+Get-Command -Verb new -Noun *Item*
+
+Get-Help -Name new-item -full
+
+New-Item -Path C:\ -Name Temp3 -ItemType Directory
+#Olusturdugunuz bu klasörü powershell ile silen komutu bulun ve calıstırın.
+Remove-Item -Path C:\Temp3 
+
+Get-Service
---
+++ Day1-Part2.ps1
@@ -0,0 +1,34 @@
+﻿Get-Service -Name ALG
+Start-Service -Name ALG
+
+Get-Service -Name ALG | Start-Service 
+
+
+Get-Service -Name ALG,BITS,sense
+
+Get-Service -Name ALG
+Get-Service -Name ALG | Get-Member
+Get-Process | Get-Member
+
+Get-Service | Sort-Object -Property Status
+Get-Process | Sort-Object -Property id
+Get-Process | Sort-Object -Property StartTime -Descending
+
+Get-Service | Get-Member
+Get-Service | Sort-Object -Property StartType
+
+Get-Service | Group-Object -Property Starttype
+
+Get-Service | Group-Object -Property Status
+
+#Makine üzerinde processleri isme göre tersten sıralayan komutu yazalım.
+Get-Process
+Get-Process | Sort-Object -Property ProcessName -Descending
+#Makine üzerinde Firewall kurallarını enable durumuna göre guplayan komutu yazalım.
+Get-NetFirewallRule | Get-Member
+Get-NetFirewallRule | Group-Object -Property Enabled
+#Makine üzerinde C:\ altında bulunan dosyaları son yazılma tarihine göre tersten sıralayalım.
+Get-ChildItem -Path C:\ | Get-Member
+
+Get-ChildItem -Path C:\ -Recurse | Sort-Object -Property LastWriteTime -Descending
+#
